b2e0c69b03151dff8b16b2e21bdfdc95687659a6
Yhonatan Jesus <yiquiapaza@gmail.com>
2022-02-21 22:48:17 -0300

    Resolve all examples

--- index.js
+++ index.js
@@ -49,17 +49,17 @@ console.log(oh.getWarnings(new Date("February 15, 2022 12:15:30")));
 
 // Intgerval could be mouths, weeks and years
 // when the date starts
-let from = new Date("01 Jan 2022");
+//let from = new Date("01 Jan 2022");
 
 // when the date ends
-let to = new Date("01 Feb 2022");
+//let to = new Date("01 Feb 2022");
 
 console.log("Opening Hours: " + oh);
 
-let intervals = oh.getOpenIntervals(from, to);
+//let intervals = oh.getOpenIntervals(from, to);
 
-for (var days in intervals)
-  console.log(oh.getState(intervals[days][0]));
+//for (var days in intervals)
+//  console.log(oh.getState(intervals[days][0]));
 
 
 console.log("Example 1");
@@ -71,8 +71,39 @@ const example2 = new opening_hours("Sa-Su 0:00-24:00");
 console.log(example2.getState());
 
 console.log("Example 3");
-const example3 = new opening_hours("Mo-Su 08:00-18:00; Apr 10-15 off; Jun 08:00-14:00; Aug off; Dec 25 off");
-console.log(example3.getState());
+const example3a = new opening_hours("Mo-Su 08:00-18:00; Apr 10-15 off; Jun 08:00-14:00; Aug off; Dec 25 off");
+const example3b = new opening_hours("Mo 10:00-12:00,12:30-15:00; Tu-Fr 08:00-12:00,12:30-15:00; Sa 08:00-12:00");
+console.log(example3a.getState());
+console.log(example3b.getState());
 
+console.log("Example 4");
+const example4a = new opening_hours("Mo-Sa 10:00-20:00; Mo off")
+console.log(example4a.getState());
+const example4b = new opening_hours("Mo-Sa 10:00-20:00; Tu 10:00-14:00")
+console.log(example4b.getState());
+
+console.log("Example 5");
+const example5 = new opening_hours("sunrise-sunset");
+console.log(example5.getState());
+
+console.log("Example 6");
+const example6 = new opening_hours("Mo 16:00+");
+console.log(example6.getState());
+console.log(example6.getUnknown());
+console.log(example6.getStateString());
+console.log(example6.getComment());
+
+console.log("Example 7");
+const example7 = new opening_hours("Apr-Oct: Fr-Su 10:00-18:00");
+console.log(example7.getState()); 
+
+console.log("Example 8");
+const example8 = new opening_hours("week 1-53/2 Fr 09:00-12:00; week 2-52/2 We 09:00-12:00");
+console.log(example8.getState());
+
+console.log("Exampole 9");
+const example9 = new opening_hours("Mo-Sa 08:00-13:00,14:00-17:00 || by appointment");
+console.log(example9.getComment());
+console.log(example9.getState());
 
 
